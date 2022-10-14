d4f34babc3cfbc7f8f431108bab8d5e7c5135a73
Pritam Mandal <103989209+Pritam04Mandal@users.noreply.github.com>
2022-10-14 23:19:19 +0530

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 using namespace std;
+int* XOR(int *divisor,int *dividend,int d1,int d2)
+{
+    int rem1[d2];
+    int *rem2=new int[d2-1];
+    for(int i=0;i<d2;i++)
+    {
+        rem1[i]=*(divisor+i);
+    }
+    for(int i=d2;i<(d1+d2-1);i++)
+    {
+        for(int j=0;j<d2-1;j++)
+        {
+            if(rem1[j+1]==*(dividend+j+1))
+            {
+                rem2[j]=1;
+            }
+            else{
+                rem2[j]=0;
+            }
+        }
+        for(int j=0;j<d2-1;j++)
+        {
+            rem1[j]=rem2[j];
+        }
+        rem1[d2-1]=*(divisor+i);
+    }
+    return rem2;
+}
 void sender(int *a,int *b,int a1, int b1)
 {
     int s[a1+b1-1];
-    int rem1[b1],rem2[b1];
     for(int i=0;i<a1;i++)
     {
-        s[i]=a[i];
+        s[i]=*(a+i);
     }
     for (int i = 0; i < b1-1; i++)
     {
         s[i]=0;
     }
-    for(int i=0;i<a1;i++)
+    int *remainder=XOR(s,b,a1,b1);
+    for(int i=0;i<b1-1;i++)
     {
-        int *remainder=XOR(a)
+        cout<<*(remainder+i);
     }
-    
+    cout<<endl;
 }
+
 void reciever();
-int* XOR()
-{
-    
-}
 int main()
 {
     int n1;
@@ -52,7 +77,7 @@ int main()
             cin>>generator[i];
         }
     }
-    sender(message,generator,n1,n2);
+    
     for (int i = 0; i < n1; i++)
     {
         cout<<message[i];
@@ -63,5 +88,6 @@ int main()
         cout<<generator[i];
     }
     cout<<endl;
+    sender(message,generator,n1,n2);
     return 0;
 }
