88b350437d21e53eee0547a830d43147191d7884
Pritam Mandal <103989209+Pritam04Mandal@users.noreply.github.com>
2022-10-16 17:24:50 +0530

    Update CRC.cpp

--- CRC.cpp
+++ CRC.cpp
@@ -3,28 +3,49 @@ using namespace std;
 int* XOR(int *divisor,int *dividend,int d1,int d2)
 {
     int rem1[d2];
-    int *rem2=new int[d2-1];
+    int extra[d2];
     for(int i=0;i<d2;i++)
     {
-        rem1[i]=*(divisor+i);
+        extra[i]=0;
     }
-    for(int i=d2;i<(d1+d2-1);i++)
+    int* rem2=new int(d2-1);
+    for(int i=0;i<d2;i++)
     {
-        for(int j=0;j<d2-1;j++)
+        rem1[i]=divisor[i];
+    }
+    for(int i=d2;i<d1+1;i++)
+    {
+        if(rem1[0]==0)
         {
-            if(rem1[j+1]==*(dividend+j+1))
+            for(int j=0;j<d2-1;j++)
             {
-                rem2[j]=1;
+                if(rem1[j+1]!=extra[j+1])
+                {
+                    rem2[j]=1;
+                }
+                else{
+                    rem2[j]=0;
+                }
             }
-            else{
-                rem2[j]=0;
+        }
+        else{
+            for(int j=0;j<d2-1;j++)
+            {
+                if(rem1[j+1]!=dividend[j+1])
+                {
+                    rem2[j]=1;
+                }
+                else{
+                    rem2[j]=0;
+                }
             }
         }
         for(int j=0;j<d2-1;j++)
         {
             rem1[j]=rem2[j];
         }
-        rem1[d2-1]=*(divisor+i);
+        rem1[d2-1]=divisor[i];
+        
     }
     return rem2;
 }
@@ -35,19 +56,51 @@ void sender(int *a,int *b,int a1, int b1)
     {
         s[i]=*(a+i);
     }
-    for (int i = 0; i < b1-1; i++)
+    for (int i = a1; i < a1+b1-1; i++)
     {
         s[i]=0;
     }
-    int *remainder=XOR(s,b,a1,b1);
+    int *remainder=XOR(s,b,a1+b1-1,b1);
+    cout<<"The Remainder is: ";
     for(int i=0;i<b1-1;i++)
     {
-        cout<<*(remainder+i);
+        cout<<remainder[i];
     }
     cout<<endl;
+    int l=0;
+    for (int i = a1; i < a1+b1-1; i++)
+    {
+        if(s[i]==remainder[l])
+        {
+            s[i]=0;
+        }
+        else{
+            s[i]=1;
+        }
+    }
+    cout<<"Transmitted message: ";
+    for (int i = 0; i < a1+b1-1; i++)
+    {
+        cout<<s[i];
+    }
+    cout<<endl;
+    reciever(s,b,a1,b1);
 }
 
-void reciever();
+void reciever(int *recieved,int *generator,int a, int b)
+{
+    int *check=XOR(recieved,generator,a+b-1,b);
+    for(int i=0;i<b-1;i++)
+    {
+        if(check[i]!=0)
+        {
+            cout<<"error in message"<<endl;
+            return;
+        }
+    }
+    cout<<"No error in transmitted message"<<endl;
+    return;
+}
 int main()
 {
     int n1;
